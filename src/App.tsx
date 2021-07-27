a81044504552a74f99a27923a4e73bc5bcaf12b6
Michel Bras <michelbras1990@gmail.com>
2021-07-27 22:50:00 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 
 const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <p>Hello world</p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <p>About</p>
+          </Route>
+          <Route path="/users">
+            <p>Users</p>
+          </Route>
+          <Route path="/">
+            <p>Home</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
