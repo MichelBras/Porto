43695376efecab33c391323afff47a825fd96666
Michel Bras <michelbras1990@gmail.com>
2021-07-27 22:24:47 +0200

    initial configuration

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      minHeight: {
+        '1px': '1px',
+        '200px': '200px',
+        '500px': '500px'
+      }
+    },
+  },
+  screens: {
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
